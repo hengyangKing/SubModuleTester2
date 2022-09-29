5460dd588de6ea0181a3b7ba85c7b8c04686e4eb
jinhengyang <jinhengyang@camera360.com>
2022-09-29 14:38:26 +0800

    git status 10

--- SubModuleTester2/SubModuleTester2/ViewController.swift
+++ SubModuleTester2/SubModuleTester2/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let _ = "submodule is nothing to submit 9."
+        let _ = "submodule is nothing to submit 10."
     }
 }
 
